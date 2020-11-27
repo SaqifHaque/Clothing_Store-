f639c367bb2d5ab0ca0a38f07f6d75e689ca3334
SaqifHaque <saqifhaque@gmail.com>
2020-11-27 23:43:06 +0600

    updated sql

--- clothing_storeV2.sql
+++ clothing_storeV2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2020 at 12:30 PM
+-- Generation Time: Nov 27, 2020 at 06:41 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -21,6 +21,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `clothing_store`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carts`
+--
+
+CREATE TABLE `carts` (
+  `cart_Id` int(5) NOT NULL,
+  `p_Id` int(5) NOT NULL,
+  `u_Id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `carts`
+--
+
+INSERT INTO `carts` (`cart_Id`, `p_Id`, `u_Id`) VALUES
+(2, 3, 0),
+(3, 6, 0),
+(4, 1, 1),
+(5, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -47,6 +69,21 @@ INSERT INTO `category` (`c_Id`, `c_name`, `gender`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `inv_Id` int(11) NOT NULL,
+  `products` varchar(100) NOT NULL,
+  `total` varchar(100) NOT NULL,
+  `card` varchar(100) NOT NULL,
+  `date` varchar(100) NOT NULL,
+  `u_Id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `notices`
 --
 
@@ -64,6 +101,19 @@ CREATE TABLE `notices` (
 INSERT INTO `notices` (`n_Id`, `n_details`, `posted_by`, `date`) VALUES
 (1, 'Welcome to Clothing Store', 'Admin', '11/11/20'),
 (2, 'Choose Your favorite porduct and purchase it seemlessly.', 'Admin', '11/11/20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `o_Id` int(5) NOT NULL,
+  `products` varchar(100) NOT NULL,
+  `total` varchar(100) NOT NULL,
+  `u_Id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -87,7 +137,13 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`p_Id`, `p_name`, `price`, `size`, `p_description`, `p_image`, `c_Id`) VALUES
 (1, 'Addidas Pant', '500', 'S,M,XL', 'Very Comfy Pants ', '', 2),
-(2, 'Jamdani Shari ', '1000', 'S,M,XL', 'Very silky shari', '', 4);
+(2, 'Jamdani Shari ', '1000', 'S,M,XL', 'Very silky shari', '', 4),
+(3, 'Puma Pants', '500', 'S,M,XL', 'Comfy Pants', '', 2),
+(4, 'Silk Shari Premium ', '2000', 'S,M,XL', 'Premium Shari', '', 4),
+(5, 'Gucci Shirt', '1500', 'S,M,XL', 'Comfy shirt', '', 1),
+(6, 'Raymond Shirt', '1800', 'S,M,XL', 'Premium Formal Shirt', '', 1),
+(7, 'Pakistani Salwar', '800', 'S,M,XL', 'Beautiful Salwar', '', 3),
+(8, 'Indian Salwar', '1000', 'S,M,XL', 'Beautiful and luxurious salwar', '', 3);
 
 -- --------------------------------------------------------
 
@@ -109,8 +165,21 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `bloodgroup`, `phonenumber`, `password`, `profilepic`, `type`, `status`, `gender`) VALUES
+(1, 'Saqif', 'saqifhaque@gmail.com', 'B+', '01845435118', 'MTIzNA==', '', 'User', 'Verified', 'Male');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `carts`
+--
+ALTER TABLE `carts`
+  ADD PRIMARY KEY (`cart_Id`);
 
 --
 -- Indexes for table `category`
@@ -119,10 +188,22 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`c_Id`);
 
 --
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`inv_Id`);
+
+--
 -- Indexes for table `notices`
 --
 ALTER TABLE `notices`
   ADD PRIMARY KEY (`n_Id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`o_Id`);
 
 --
 -- Indexes for table `products`
@@ -142,10 +223,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `carts`
+--
+ALTER TABLE `carts`
+  MODIFY `cart_Id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
   MODIFY `c_Id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `inv_Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `notices`
@@ -154,16 +247,22 @@ ALTER TABLE `notices`
   MODIFY `n_Id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `o_Id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `p_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `p_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
